4024e1ba9852286f30e9a3fadb9b1ec94bde3adb
Emilia Brzezińska <94071705+emiliabrzezinska@users.noreply.github.com>
2022-01-11 13:37:42 +0100

    created components

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,9 +1,13 @@
-
+import { Form } from "./compontents/Form";
+import { Filter } from "./compontents/Filter";
+import { Tasks } from "./compontents/Tasks";
 
 function App() {
   return (
     <div className="App">
-    
+      <Form></Form>
+      <Filter></Filter>
+      <Tasks></Tasks>
     </div>
   );
 }

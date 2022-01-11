8d396ca0b94e10effae17fd86fee82928617af18
Emilia Brzezińska <94071705+emiliabrzezinska@users.noreply.github.com>
2022-01-11 15:09:48 +0100

    css wip

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,16 +1,29 @@
 import { Form } from "./compontents/Form";
 import { Filter } from "./compontents/Filter";
 import { Tasks } from "./compontents/Tasks";
+import styled from "styled-components";
+
+
+const StyledDiv = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  padding: 0px;
+  background-color: #f5f5f5;
+
+`
 
 function App() {
   return (
-    <div className="App">
+    <StyledDiv>
       <Form></Form>
       <Tasks></Tasks>
       <Tasks></Tasks>
       <Tasks></Tasks>
       <Filter></Filter>
-    </div>
+    </StyledDiv>
   );
 }
 
--- todo/src/compontents/Form.js
+++ todo/src/compontents/Form.js
@@ -1,13 +1,46 @@
 import React from "react";
+import styled from 'styled-components';
+
+
+
+
+const StyledH1 = styled.h1`
+    font-size: 100px;
+    font-weight: 400;
+    color: rgba(175, 47, 47, 0.15);
+    text-align: center;
+`
+
+const StyledButton = styled.button`
+    border: 0px;
+`
+
+const StyledInput = styled.input`
+    border: 0px;
+    padding: 16px 16px 16px 60px;
+    box-shadow:inset 0 -2px 1px rgb(0 0 0 / 3%);
+    width: 550px;
+    height: 66px;
+    font-size: 24px;
+    line-height: 1.4em;
+    color: #4d4d4d;
+    &::placeholder {
+        font-style: italic;
+        color: lightgray;
+    }
+    &:focus {
+        outline: 0;
+    }
+`
 
 export const Form = () => {
     return ( 
         <>
             <form>
                 <label htmlFor="add-task">
-                    <h1>todos</h1>
-                    <button>a</button>
-                    <input type="text" name="add-task" placeholder="What needs to be done?"></input>
+                    <StyledH1>todos</StyledH1>
+                    <StyledButton>></StyledButton>
+                    <StyledInput type="text" name="add-task" placeholder="What needs to be done?"></StyledInput>
                 </label>
             </form>
         </>

179deff1b20b6e6d190b184afe93a5ed8a9a2c7a
Mariusz Walusiak <marwal91@o2.pl>
2022-01-13 22:49:11 +0100

    css checkbox styled

--- todo/src/compontents/Tasks.js
+++ todo/src/compontents/Tasks.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import styles from "./styles/Tasks.module.css"
-
+import React from "react";
+import { useState } from "react";
+import styles from "./styles/Tasks.module.css";
 
 export const Tasks = () => {
-    return (
-        <li className={styles.li}>
-                <input type="checkbox" name="task"></input>
-                <label htmlFor='task' className={styles.label}>Label...</label>
-                <button className={styles.button}>x</button>
-        </li>
-    )
-}
+  const [checked, setChecked] = useState(false);
+
+  return (
+    <li className={styles.li}>
+      <div className={styles.container}>
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={() => console.log("this is change!")}
+        ></input>
+        <label
+          onClick={() => setChecked((was) => !was)}
+          htmlFor="checkbox"
+        ></label>
+      </div>
+      <span className={styles.label}>Label</span>
+
+      <button className={styles.button}>x</button>
+    </li>
+  );
+};

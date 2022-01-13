072e3ab0b901f1ed75ee3bf50b7e3d32e0868b4d
Mariusz Walusiak <marwal91@o2.pl>
2022-01-13 15:16:40 +0100

    styles in progress II

--- todo/src/compontents/Filter.js
+++ todo/src/compontents/Filter.js
@@ -1,12 +1,17 @@
 import React from "react"
+import styles from './styles/Filter.module.css'
+
+
 
 export const Filter = () => {
     return (
-        <div>
+        <div className={styles.filter}>
             <span>0 items left</span>
-            <button>All</button>
-            <button>Active</button>
-            <button>Completed</button>
+            <div>
+                <button >All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
             <button>Clear completed</button>
         </div>
     )
